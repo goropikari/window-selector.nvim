2d09def4428d74f4a00ab5eee6bf3a9ae401c277
goropikari <goropikari56@gmail.com>
2024-12-05 02:44:16 +0900

    add setup

--- lua/sample/init.lua
+++ lua/sample/init.lua
@@ -1,7 +1,16 @@
 local M = {}
 
+local default_config = {
+  greet = 'hello',
+}
+local global_config = {}
+
 function M.hello()
-  print('Hello World!')
+  print(global_config.greet)
+end
+
+function M.setup(opts)
+  global_config = vim.tbl_deep_extend('force', default_config, opts or {})
 end
 
 return M
